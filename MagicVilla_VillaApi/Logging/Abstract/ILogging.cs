ed7cbf9778a5cf50125ce4d3ca130e39e7f7491e
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-19 00:30:47 +0400

    Wrote Custom Logging

---
+++ MagicVilla_VillaApi/Logging/Abstract/ILogging.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicVilla_VillaApi.Logging.Abstract
+{
+    public interface ILogging<T>
+    {
+        void Log(string message,string type);   
+    }
+}
---
+++ MagicVilla_VillaApi/Logging/Concrete/Logging.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla_VillaApi.Logging.Abstract;
+
+namespace MagicVilla_VillaApi.Logging.Concrete
+{
+    public class Logging<T> : ILogging<T>
+    {
+        public void Log(string message, string type)
+        {
+           
+            if (type== "Info")
+            {
+                Console.WriteLine(type+": "+message);
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+            if (type == "Error")
+            {
+                Console.WriteLine(type + ": " + message);
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+
+        }
+    }
+}
