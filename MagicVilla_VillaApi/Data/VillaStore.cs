ff1ea7dc41a973c0dc6360c1d84263529019ddac
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-19 00:32:20 +0400

    Add data storing static class

---
+++ MagicVilla_VillaApi/Data/VillaStore.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaApi.Models.Dtos;
+using System.Xml.Linq;
+
+namespace MagicVilla_VillaApi.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto>()
+        {
+                new VillaDto() { Name = "Balaxani", Id = 1,Occupancy=3,Sqft=100},
+                new VillaDto() { Id = 2,Name = "Mastaga",Occupancy=4,Sqft=300}
+        };
+    
+    }
+}
