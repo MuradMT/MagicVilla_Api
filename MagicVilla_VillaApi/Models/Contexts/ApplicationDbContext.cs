5c1c9ff23f8ad0caa350e8e3aef27d7a7ffff68c
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-19 00:31:47 +0400

    Add entity framewwork dbcontext

---
+++ MagicVilla_VillaApi/Models/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaApi.Models.Contexts
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Villa> Villas { get; set; }    
+    }
+}
