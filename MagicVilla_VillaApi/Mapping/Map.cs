97852a8b2516c5c7d79bcd3c75b0b9ca16674ad4
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-19 15:00:15 +0400

    Add Mapper class

---
+++ MagicVilla_VillaApi/Mapping/Map.cs
@@ -0,0 +1,39 @@
+﻿using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Dtos;
+
+namespace MagicVilla_VillaApi.Mapping
+{
+    public static class Map
+    {
+        public static Villa VillaMapper(VillaDto villaDto)
+        {
+            Villa model = new Villa()
+            {
+                Name = villaDto.Name,
+                Id = villaDto.Id,
+                Sqft = villaDto.Sqft,
+                Amenity = villaDto.Amenity,
+                Details = villaDto.Details,
+                ImageUrl = villaDto.ImageUrl,
+                Occupancy = villaDto.Occupancy,
+                Rate = villaDto.Rate
+            };
+            return model;
+        }
+        public static VillaDto VillaDtoMapper(Villa villa)
+        {
+            VillaDto model = new VillaDto()
+            {
+                Name = villa.Name,
+                Id = villa.Id,
+                Sqft = villa.Sqft,
+                Amenity = villa.Amenity,
+                Details = villa.Details,
+                ImageUrl = villa.ImageUrl,
+                Occupancy = villa.Occupancy,
+                Rate = villa.Rate
+            };
+            return model;
+        }
+    }
+}
