eeade0cdcb6395b348a92d43f4599977e18b1fae
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-22 22:36:12 +0400

    Add new VillaNumber Repository

---
+++ MagicVilla_VillaApi/Repositories/VillaNumberRepository.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Contexts;
+using MagicVilla_VillaApi.Repositories.IRepository;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaApi.Repositories
+{
+    public class VillaNumberRepository : Repository<VillaNumber>, IVillaNumberRepository
+    {
+        private ApplicationDbContext _context;
+        public VillaNumberRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<VillaNumber> UpdateAsync(VillaNumber entity)
+        {
+            entity.UpdatedTime = DateTime.Now;
+             _context.VillaNumbers.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+            
+        }
+    }
+}
