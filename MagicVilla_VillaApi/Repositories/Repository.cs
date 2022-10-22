1661daec9c16d8bc42f450dbcfb7a936fcf6ade0
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-22 19:14:02 +0400

    Added Generic Repository Pattern

---
+++ MagicVilla_VillaApi/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿using MagicVilla_VillaApi.Models.Contexts;
+using MagicVilla_VillaApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using MagicVilla_VillaApi.Repositories.IRepository;
+
+namespace MagicVilla_VillaApi.Repositories
+{
+    public class Repository<T>:IRepository<T> where T :class
+    {
+        private ApplicationDbContext _context;
+        internal DbSet<T> dbSet { get; set; }    
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet=_context.Set<T>();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync(filter);
+
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync() :
+               
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            dbSet.Remove(entity);
+            await SaveAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+       
+    }
+}
--- MagicVilla_VillaApi/Repositories/VillaRepository.cs
+++ MagicVilla_VillaApi/Repositories/VillaRepository.cs
@@ -7,61 +7,21 @@ using System.Linq.Expressions;
 
 namespace MagicVilla_VillaApi.Repositories
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository :Repository<Villa>,IVillaRepository
     {
         private ApplicationDbContext _context;
-        public VillaRepository(ApplicationDbContext context)
+        public VillaRepository(ApplicationDbContext context):base(context)
         {
             _context = context;
         }
 
-        public async Task CreateAsync(Villa entity)
+        //update  hemise ayri yazilar
+        public async Task<Villa> UpdateAsync(Villa entity)
         {
-            await _context.Villas.AddAsync(entity);
-            await SaveAsync();
-        }
-
-        public async Task<Villa> GetAsync(Expression<Func<Villa,bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Villa> query = _context.Villas;
-            if (!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-            if(filter != null)
-            {
-                query=query.Where(filter);
-            }
-            return await query.FirstOrDefaultAsync(filter);
-                
-        }
-
-        public async Task<List<Villa>> GetAllAsync(Expression<Func<Villa,bool>> filter = null)
-        {
-            IQueryable<Villa> query = _context.Villas;
-            if(filter != null)
-            {
-                query = query.Where(filter);
-            }
-            return  await query.ToListAsync() :
-               
-        }
-
-        public async Task RemoveAsync(Villa entity)
-        {
-            _context.Villas.Remove(entity);    
-            await SaveAsync();
-        }
-
-        public async Task SaveAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(Villa entity)
-        {
+            entity.UpdatedDate = DateTime.Now;
            _context.Villas.Update(entity);
-            await SaveAsync();
+            await _context.SaveChangesAsync();
+            return entity;
         }
     }
 }
