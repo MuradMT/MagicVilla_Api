96d933b274d6bfa97839f8e0cdafbb70aa9132fc
Murad Mammadzade <mammadzade03@gmail.com>
2022-11-09 21:53:17 +0400

    Add repository and also JWT  Token

---
+++ MagicVilla_VillaApi/Repositories/IRepository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Dtos;
+
+namespace MagicVilla_VillaApi.Repositories.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<LocalUser> Register(RegistrationRequestDto registrationRequestDto);
+
+    }
+}
