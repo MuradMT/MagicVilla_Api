60496f7b6dbc79886c753a185dcf097eb7738677
Murad Mammadzade <mammadzade03@gmail.com>
2022-11-09 21:51:56 +0400

    Add database users

--- MagicVilla_VillaApi/Models/Contexts/ApplicationDbContext.cs
+++ MagicVilla_VillaApi/Models/Contexts/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace MagicVilla_VillaApi.Models.Contexts
         }
         public DbSet<Villa> Villas { get; set; }
         public DbSet<VillaNumber> VillaNumbers { get; set; }
+        public DbSet<LocalUser> LocalUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Villa>().HasData(
